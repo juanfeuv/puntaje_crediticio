fad3128f2eef6825c65843ef96e2a34cf7a2ed73
Juan Felipe Usuga Villegas <45887686+juanfeuv@users.noreply.github.com>
2023-09-26 21:03:46 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## Integrantes
+* Cristian Jaramillo Herrera
+* Danilo Giraldo Lopez
 * Juan Felipe Usuga Villegas
+* Maria Camila Durango Muñoz
 
 ## Consideraciones
-* Video youtube: 
-* Link APP: 
-* Reporte Técnico: 
+* Video youtube: https://www.youtube.com/watch?v=Y1YzzD0DppE
+* Link APP: https://scintillating-moxie-fde9d0.netlify.app/
+* Reporte Técnico: https://scintillating-moxie-fde9d0.netlify.app/Informe.html
--- src/componentes/Toolbar/Toolbar.jsx
+++ src/componentes/Toolbar/Toolbar.jsx
@@ -12,23 +12,26 @@ const Toolbar = () => {
 
   return (
     <Navbar bg="primary" variant="dark" style={{ position: 'fixed', width: '100%', zIndex: '1000' }}>
-      <Navbar.Brand href="/">Puntaje crediticio</Navbar.Brand>
+      <Navbar.Brand href="/"> <img src="/icon.jpg" style={{ width: '50px', height: '50px' }} /> Puntaje crediticio</Navbar.Brand>
       <Nav className="mr-auto">
         {/* <Nav.Link href="/">Home</Nav.Link> */}
       </Nav>
       <Form inline>
-      <Button variant="outline-light" href='' target="_blank" rel="noreferrer">Reporte</Button>
+      <Button variant="outline-light" href='/Informe.html' target="_blank" rel="noreferrer">Reporte</Button>
         &nbsp;
         <Button variant="outline-light" href='https://github.com/juanfeuv/puntaje_crediticio' target="_blank" rel="noreferrer">Repositorio</Button>
         &nbsp;
-        <Button variant="outline-light" href='' target="_blank" rel="noreferrer">Video</Button>
+        <Button variant="outline-light" href='https://www.youtube.com/watch?v=Y1YzzD0DppE' target="_blank" rel="noreferrer">Video</Button>
         &nbsp;
         <Button variant="outline-light" ref={target} onClick={() => setShow(!show)}>Integrantes</Button>
         <Overlay target={target.current} show={show} placement="bottom" >
           {(props) => (
             <Tooltip id="overlay-example" {...props}>
               <ul>
+                <li>Cristian Jaramillo Herrera</li>
+                <li>Danilo Giraldo Lopez</li>
                 <li>Juan Felipe Usuga Villegas</li>
+                <li>Maria Camila Durango Muñoz</li>
               </ul>
             </Tooltip>
           )}
